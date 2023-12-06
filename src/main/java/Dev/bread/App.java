16e831b64bacb0d53d41ff075c6adc07b5329628
HP <HP@CHANDUESLAVATH>
2023-12-06 18:26:56 +0530

    Hi

---
+++ src/main/java/Dev/bread/App.java
@@ -0,0 +1,13 @@
+package Dev.bread;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
